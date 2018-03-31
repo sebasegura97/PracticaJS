9316b55665b41024f68406021596378fc9ed17c7
sebasegura97 <sebasegura97@gmail.com>
2018-03-31 14:14:41 -0300

    no funciona

--- ReactJs/index.js
+++ ReactJs/index.js
@@ -1,1 +1,12 @@
-console.log('Hola Mundo!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Media from 'src/playlist/components/media';
+
+
+const holaMundo = <h1>Hola mundo!</h1>;
+// ReactDOM.render(holaMundo, app);
+
+// ReactDOM.render(que voy a renderizar, donde lo haré);
+
+const app = document.getElementById('app')
+ReactDOM.render(<Media />, app);
